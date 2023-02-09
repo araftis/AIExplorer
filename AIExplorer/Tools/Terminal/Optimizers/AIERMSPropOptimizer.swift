1530e8d8057ff851434169fd14471e6abd15bd67
AJ Raftis <raftisalex@gmail.com>
2023-02-08 19:44:27 -0800

    A lot of work on getting optimizers working.

--- AIExplorer/Tools/Terminal/Optimizers/AIERMSPropOptimizer.swift
+++ AIExplorer/Tools/Terminal/Optimizers/AIERMSPropOptimizer.swift
@@ -4,4 +4,45 @@ import Draw
 @objcMembers
 open class AIERMSPropOptimizer: AIEOptimizer {
 
+    /// A hyper-parameter that specifies the momentum factor.
+    var momentumScale: Float = 0.0
+    /// The constant for smoothing.
+    var alpha: Float = 0.99
+    /// The epsilon value you use to improve numerical stability.
+    var epsilon: Float = 1e-8
+    /// A Boolean that indicates whether you compute the centered RMSProp.
+    var isCentered: Bool = false
+    
+    // MARK: - AJRXMLCoding
+    
+    open override func decode(with coder: AJRXMLCoder) {
+        coder.decodeFloat(forKey: "momentumScale") { value in
+            self.momentumScale = value
+        }
+        coder.decodeFloat(forKey: "alpha") { value in
+            self.alpha = value
+        }
+        coder.decodeFloat(forKey: "epsilon") { value in
+            self.epsilon = value
+        }
+        coder.decodeBool(forKey: "isCentered") { value in
+            self.isCentered = value
+        }
+    }
+    
+    open override func encode(with coder: AJRXMLCoder) {
+        if momentumScale != 0.9 {
+            coder.encode(momentumScale, forKey: "momentumScale")
+        }
+        if alpha != 0.999 {
+            coder.encode(alpha, forKey: "alpha")
+        }
+        if epsilon != 1e-07 {
+            coder.encode(epsilon, forKey: "epsilon")
+        }
+        if isCentered {
+            coder.encode(isCentered, forKey: "isCentered")
+        }
+    }
+    
 }
--- AIExplorer/Tools/Terminal/Optimizers/AIESGDOptimizer.swift
+++ AIExplorer/Tools/Terminal/Optimizers/AIESGDOptimizer.swift
@@ -3,5 +3,30 @@ import Draw
 
 @objc
 open class AIESGDOptimizer: AIEOptimizer {
-
+    
+    /// A hyper-parameter that specifies the momentum factor.
+    var momentumScale: Float = 0.0
+    /// The constant for smoothing.
+    var usesNesterovMomentum: Bool = false
+    
+    // MARK: - AJRXMLCoding
+    
+    open override func decode(with coder: AJRXMLCoder) {
+        coder.decodeFloat(forKey: "momentumScale") { value in
+            self.momentumScale = value
+        }
+        coder.decodeBool(forKey: "usesNesterovMomentum") { value in
+            self.usesNesterovMomentum = value
+        }
+    }
+    
+    open override func encode(with coder: AJRXMLCoder) {
+        if momentumScale != 0.9 {
+            coder.encode(momentumScale, forKey: "momentumScale")
+        }
+        if usesNesterovMomentum {
+            coder.encode(usesNesterovMomentum, forKey: "usesNesterovMomentum")
+        }
+    }
+    
 }
